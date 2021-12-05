bf409abc312c00bb0357bacee367d938e8dd6bf8
DESKTOP-7H7C8K6\Piratchai-mac-dev <pong_bb1@hotmail.com>
2021-12-05 20:28:11 +0700

    Add project files.

---
+++ WinFormTesseract/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace WinFormTesseract
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSlcImage = new System.Windows.Forms.Button();
+            this.txtBoxImagePath = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnConvertToText = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.14286F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(605, 85);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Convert Image";
+            // 
+            // btnSlcImage
+            // 
+            this.btnSlcImage.Location = new System.Drawing.Point(178, 170);
+            this.btnSlcImage.Name = "btnSlcImage";
+            this.btnSlcImage.Size = new System.Drawing.Size(181, 53);
+            this.btnSlcImage.TabIndex = 1;
+            this.btnSlcImage.Text = "Select Image";
+            this.btnSlcImage.UseVisualStyleBackColor = true;
+            this.btnSlcImage.Click += new System.EventHandler(this.btnSlcImage_Click);
+            // 
+            // txtBoxImagePath
+            // 
+            this.txtBoxImagePath.Location = new System.Drawing.Point(32, 254);
+            this.txtBoxImagePath.Name = "txtBoxImagePath";
+            this.txtBoxImagePath.ReadOnly = true;
+            this.txtBoxImagePath.Size = new System.Drawing.Size(511, 29);
+            this.txtBoxImagePath.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(32, 311);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(511, 241);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(803, 312);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(652, 241);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // btnConvertToText
+            // 
+            this.btnConvertToText.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvertToText.Location = new System.Drawing.Point(1034, 170);
+            this.btnConvertToText.Name = "btnConvertToText";
+            this.btnConvertToText.Size = new System.Drawing.Size(181, 53);
+            this.btnConvertToText.TabIndex = 5;
+            this.btnConvertToText.Text = "Convert Image";
+            this.btnConvertToText.UseVisualStyleBackColor = true;
+            this.btnConvertToText.Click += new System.EventHandler(this.btnConvertToText_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 624);
+            this.Controls.Add(this.btnConvertToText);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtBoxImagePath);
+            this.Controls.Add(this.btnSlcImage);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSlcImage;
+        private System.Windows.Forms.TextBox txtBoxImagePath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnConvertToText;
+    }
+}
+
---
+++ WinFormTesseract/Form1.cs
@@ -0,0 +1,62 @@
+﻿using IronOcr;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+
+namespace WinFormTesseract
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSlcImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+
+            // image filters
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png)|*.jpg; *.jpeg; *.gif; *.bmp; *.png";
+
+            if(open.ShowDialog() == DialogResult.OK)
+            {
+                // display image in picture box
+                pictureBox1.Image = new Bitmap(open.FileName);
+
+                // image file path
+                txtBoxImagePath.Text = open.FileName;
+            }
+        }
+
+        private void btnConvertToText_Click(object sender, EventArgs e)
+        {
+            //IronTesseract IronOcr = new IronTesseract();
+
+            //var r = IronOcr.Read(txtBoxImagePath.Text);
+
+            //richTextBox1.Text = r.Text;
+
+            // -- use tesseract -- //
+            try
+            {
+                var ocrengine = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default);
+                var img = Pix.LoadFromFile(txtBoxImagePath.Text);
+                var res = ocrengine.Process(img);
+                richTextBox1.Text = res.GetText();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message);
+            }
+            
+        }
+    }
+}
